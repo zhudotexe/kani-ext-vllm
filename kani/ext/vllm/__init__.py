4745e9fb151e9c12a5fb26c0900d7a5645f80810
Andrew Zhu <me@andrew-zhu.com>
2025-10-30 12:43:11 -0400

    build: ok, maybe no lazy imports for now

--- kani/ext/vllm/__init__.py
+++ kani/ext/vllm/__init__.py
@@ -1,23 +1,3 @@
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from .engine import VLLMEngine
-    from .http_engine import VLLMServerEngine
-    from .openai_compat_engine import VLLMOpenAIEngine
-else:
-    # lazy-load the engines to avoid expensive imports
-    def __getattr__(name):
-        match name:
-            case "VLLMEngine":
-                from .engine import VLLMEngine
-
-                return VLLMEngine
-            case "VLLMServerEngine":
-                from .http_engine import VLLMServerEngine
-
-                return VLLMServerEngine
-            case "VLLMOpenAIEngine":
-                from .openai_compat_engine import VLLMOpenAIEngine
-
-                return VLLMOpenAIEngine
-        raise AttributeError(name=name)
+from .engine import VLLMEngine
+from .http_engine import VLLMServerEngine
+from .openai_compat_engine import VLLMOpenAIEngine
