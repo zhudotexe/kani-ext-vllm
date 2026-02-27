c3a6c779ebc54b13c89d647ae9c00db4af545eda
Andrew Zhu <me@andrew-zhu.com>
2026-02-27 14:00:18 +0900

    feat: cli provider

--- kani/ext/vllm/__init__.py
+++ kani/ext/vllm/__init__.py
@@ -1,3 +1,8 @@
 from .engine import VLLMEngine
 from .http_engine import VLLMServerEngine
 from .openai_compat_engine import VLLMOpenAIEngine
+
+# cli
+CLI_PROVIDERS = [
+    ("vllm", [], VLLMServerEngine),
+]
